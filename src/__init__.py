88f1e54beaeb17418e13f9da47d8f7a512ec7d7b
Achraf Gharbi <achraf.ghb@gmail.com>
2020-10-12 20:46:43 +0200

    make simple endpoints for auth

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object("src.config.Config")
+db = SQLAlchemy(app)
 
 from .models import *
 from .routes import *
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,35 @@
-from flask import jsonify
+from flask import jsonify, redirect, request
 from src import app
+from .spotify_auth import SpotifyAuth
 
 @app.route("/")
-def hello_world():
+def helloWorld():
     return jsonify(hello="world")
+
+@app.route("/auth/authorize")
+def authLogin():
+    spotify = SpotifyAuth()
+    redirect_link = spotify.getUser()
+    return redirect(redirect_link)
+
+@app.route("/auth/callback")
+def authCallback():
+    error = request.args.get('error')
+    if error:
+        return jsonify({
+            'status': 'error',
+            'error': error
+        }), 500
+    code = request.args.get('code')
+
+    spotify = SpotifyAuth()
+    response = spotify.getUserToken(code)
+    if "error" in response:
+        return jsonify({
+            'status': 'error',
+            'error': response
+        }), 500
+    
+    # Make the magic here :)
+    return jsonify(response)
+
